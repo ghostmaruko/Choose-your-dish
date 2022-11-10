d125c39e20a2c7db7907d2feaae98686f3fc874b
ghostmaruko <m4rc035u@gmail.com>
2022-11-10 13:33:37 +0100

    add style and complete

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/mealdetails/:id" element={<MealDetails />} />
-    </Routes>
+    </Routes> 
   );
 }
 
